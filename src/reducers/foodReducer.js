a13f71e8e40f2f40d4a2db7e565141317b0acbb5
Claudia Lujan <verenisse.eyzaguirre@gmail.com>
2022-08-14 23:42:09 -0500

    create order_items reducer

---
+++ src/reducers/foodReducer.js
@@ -0,0 +1,24 @@
+
+export const foodReducer = ( state = [] , action ) => {
+
+    switch ( action.type ) {
+        
+        case 'add':
+            return [...state, action.payload];
+
+        case 'update':
+            return state.map(
+                    product => (product.id === action.payload.id)
+                    ? {...product, qty: product.qty + 1}
+                    : product
+                )
+
+        case 'delete':
+            return state.filter( product => product.id !== action.payload ); 
+
+        default:
+            return state; 
+    }
+
+}
+   
